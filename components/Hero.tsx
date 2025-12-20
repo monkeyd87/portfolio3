6574a73a7bdcb338a81de801c40fac34fd671172
Dennis Jackson <dennisdjack1987@gmail.com>
2025-12-20 01:37:41 -0800

    fixing background

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,17 +12,19 @@ export const Hero = ()=>{
                 <Spotlight className="top-10 left-full md:-left-32 md:-top-20 h-[80vh] w-[50vw]" fill="purple"/>
                 <Spotlight className="top-28 left-80  h-[80vh] w-[50vw]  " fill="blue"/>
             </div>
-            <div className="absolute top-0 left-0  h-screen w-full flex items-center justify-center bg-black dark:bg-black">
+            <div className="absolute top-0 left-0  h-screen w-full flex items-center justify-center bg-black">
                 <div
                     className={cn(
                     "absolute inset-0",
                     "[background-size:40px_40px]",
-                    "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-                    "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+                    "[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
                     )}
                 />
                 {/* Radial gradient for the container to give a faded look */}
-                <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+                <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black">
+
+                    
+                </div>
             </div>
             <div className="flex  justify-center  relative   z-10 ">
                     <div className="max-w-[100vw] md:max-w-2xl lg:max-w-100 flex flex-col items-center justify-center">
